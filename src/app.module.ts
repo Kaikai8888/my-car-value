1870f474aa97808867e571ae3e5c63446f199ea2
Kelly Wu <kelly.wu@shoplineapp.com>
2023-04-17 21:31:26 +0800

    feat: inject entity and repository with TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report], // connect the entity to the root module
+      synchronize: true,
+    }), //the connection will be passed down to the other module below app module 
+    ReportsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], /* Connect the entity to its parent module. 
+  This creates Repository for us 
+  define which repositories are registered in the current scope. */
   controllers: [UsersController],
   providers: [UsersService]
 })
