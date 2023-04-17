f90a3759e64a621602800066e48533793c8f22e5
Kelly Wu <kelly.wu@shoplineapp.com>
2023-04-17 22:04:05 +0800

    add: validating pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true // 只會傳入有宣告的屬性, 其他屬性會被排除
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
