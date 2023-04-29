7c0c6ef7cc394073c62734f8ca98751be6ac9a05
Kelly Wu <kelly.wu@shoplineapp.com>
2023-04-29 19:29:03 +0800

    feat: add patch endpoint with update-user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Patch, Delete, Param, Body, Query } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -19,6 +20,11 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email)
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body)
   }
 
   @Delete('/:id')
