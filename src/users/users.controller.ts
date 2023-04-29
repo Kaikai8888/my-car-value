34be993b033f0d2c0db73d394e97b28e882c5e76
Kelly Wu <kelly.wu@shoplineapp.com>
2023-04-29 19:39:41 +0800

    feat: throw notFoundException when user not found

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -13,8 +13,12 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id))
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id))
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+    return user
   }
 
   @Get()
